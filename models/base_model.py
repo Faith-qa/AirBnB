11476930f515f901d88b4a13ef0cdab6f70752c1
Faith-qa <atienofaith12@gmail.com>
2022-03-06 15:33:20 +0300

    updated dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from copy import copy
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
@@ -13,17 +13,18 @@ class BaseModel:
         instance is created
         updated_at: type datetime, current date time when an instance
         is created, update everytime an opject changes
-
+    
     """
     def __init__(self, *args, **kwargs):
         """
+        
         initialize the baseModel class
         """
+        
         if not kwargs:
 
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = self.created_at = datetime.now()
         else:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -32,16 +33,20 @@ class BaseModel:
                                 datetime.datetime.fromisoformat(value))
                     else:
                         setattr(self, key, value)
+    
+    
+
 
     def __str__(self):
-        return("[{}] (<{}>) <{}>".format(__name__, self.id, self.__dict__))
+        print(self.id)
+        return("[{}] ({}) {}".format(type(self).__name__, self.id,  self.__dict__))
 
     def save(self):
         """
         updates updated_at with current date time
 
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
@@ -52,10 +57,13 @@ class BaseModel:
         the object
 
         """
-        dict_1 = self.__dict__.copy()
+        dict_1 = self.__dict__
+        print(dict_1)
         dict_1["__class__"] = self.__class__.__name__
         for k, v in self.__dict__.items():
-            if k in self.__dict__.items():
+            if k in ("created_at", "updated_at"):
+
                 v = self.__dict__[k].isoformat()
                 dict_1[k] = v
+            
         return dict_1
